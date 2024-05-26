43fd34d557be59b177c3dd01df0edeacbd228185
Gitudon <oniku.udon.029@gmail.com>
2024-05-26 13:12:02 +0900

    自動補完に対応

--- progress_manager.py
+++ progress_manager.py
@@ -9,34 +9,62 @@ s=[]
 
 def message():
     st.title("問題進捗確認")
+    st.write("AtCoderの問題の進捗を確認することができます。表が出てこなかったらボタンを連打してください。")
 
 def input_contents():
     global q, u
     #ここに自動補完するか選択を入れる
-    url1=st.text_input("問題ページのURLを入力してください(1問目)")
-    if url1!="":
-        q.append(ac.url_to_problem_name(url1))
-        u.append(url1)
-    url2=st.text_input("問題ページのURLを入力してください(2問目)")
-    if url2!="":
-        q.append(ac.url_to_problem_name(url2))
-        u.append(url2)
-    url3=st.text_input("問題ページのURLを入力してください(3問目)")
-    if url3!="":
-        q.append(ac.url_to_problem_name(url3))
-        u.append(url3)
-    url4=st.text_input("問題ページのURLを入力してください(4問目)")
-    if url4!="":
-        q.append(ac.url_to_problem_name(url4))
-        u.append(url4)
-    url5=st.text_input("問題ページのURLを入力してください(5問目)")
-    if url5!="":
-        q.append(ac.url_to_problem_name(url5))
-        u.append(url5)
-    url6=st.text_input("問題ページのURLを入力してください(6問目)")
-    if url6!="":
-        q.append(ac.url_to_problem_name(url6))
-        u.append(url6)
+    select=st.selectbox(label="選択してください",options=("手動入力","A-四則演算", "A-条件分岐","B-文字列", "B-forループ", "C-リスト", "C- 1次予選過去問","S1-whileループ","S1-多重ループ","S1-多次元リスト","S2-ソート関数","S2-再帰関数"))
+    if select=="手動入力":
+        url1=st.text_input("問題ページのURLを入力してください(1問目)")
+        if url1!="":
+            q.append(ac.url_to_problem_name(url1))
+            u.append(url1)
+        url2=st.text_input("問題ページのURLを入力してください(2問目)")
+        if url2!="":
+            q.append(ac.url_to_problem_name(url2))
+            u.append(url2)
+        url3=st.text_input("問題ページのURLを入力してください(3問目)")
+        if url3!="":
+            q.append(ac.url_to_problem_name(url3))
+            u.append(url3)
+        url4=st.text_input("問題ページのURLを入力してください(4問目)")
+        if url4!="":
+            q.append(ac.url_to_problem_name(url4))
+            u.append(url4)
+        url5=st.text_input("問題ページのURLを入力してください(5問目)")
+        if url5!="":
+            q.append(ac.url_to_problem_name(url5))
+            u.append(url5)
+        url6=st.text_input("問題ページのURLを入力してください(6問目)")
+        if url6!="":
+            q.append(ac.url_to_problem_name(url6))
+            u.append(url6)
+    else:
+        if select=="A-四則演算":
+            u=pb.A_calc
+        elif select=="A-条件分岐":
+            u=pb.A_if
+        elif select=="B-文字列":
+            u=pb.B_string
+        elif select=="B-forループ":
+            u=pb.B_for
+        elif select=="C-リスト":
+            u=pb.C_list
+        elif select=="C- 1次予選過去問":
+            u=pb.C_kakomon
+        elif select=="S1-whileループ":
+            u=pb.S1_while
+        elif select=="S1-多重ループ":
+            u=pb.S1_multi
+        elif select=="S1-多次元リスト":
+            u=pb.S1_multi_list
+        elif select=="S2-ソート関数":
+            u=pb.S2_sort
+        elif select=="S2-再帰関数":
+            u=pb.S2_recursive
+        for url in u:
+            q.append(ac.url_to_problem_name(url))
 
 def input_students():
     global s
@@ -79,31 +107,12 @@ def makegraph():
 def printgraph(df):
     st.write(df)
 
-def manual():
+def main():
+    message()
     input_contents()
     input_students()
     if st.button("進捗を確認する"):
         makegraph()
-
-def auto():
-    global q, s, u
-    course=st.radio("コースを選択してください", ("A-四則演算", "A-条件分岐","B-文字列", "B-forループ", "C-リスト", "S1-whileループ", "S1-多重ループ","S1-多次元リスト","S2-ソート関数","S2-再帰関数"))
-    if course=="A-四則演算":
-        for url in pb.A1_calc:
-            q.append(ac.url_to_problem_name(url))
-            u.append(url)
-    input_students()
-    if st.button("進捗を確認する"):
-        makegraph()
-
-def reset():
-    q.clear()
-    u.clear()
-    s.clear()
-
-def main():
-    message()
-    manual()
 
 if __name__=="__main__":
     main()

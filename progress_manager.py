11facdd904dc7203a97cf7be89f711e7178e95e0
Gitudon <oniku.udon.029@gmail.com>
2024-05-26 10:51:44 +0900

    仕様を変更

--- progress_manager.py
+++ progress_manager.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import atcoder as ac
+import problems as pb
 
 q=[]
 u=[]
@@ -10,42 +11,35 @@ def message():
     st.title("問題進捗確認")
 
 def input_contents():
-    global q, s, u
+    global q, u
+    #ここに自動補完するか選択を入れる
     url1=st.text_input("問題ページのURLを入力してください(1問目)")
     if url1!="":
         q.append(ac.url_to_problem_name(url1))
         u.append(url1)
-    else:
-        q.append(ac.url_to_problem_name("https://atcoder.jp/contests/abc001/tasks/abc001_1")+" (サンプル問題1)")
-        u.append("https://atcoder.jp/contests/abc001/tasks/abc001_1")
     url2=st.text_input("問題ページのURLを入力してください(2問目)")
     if url2!="":
         q.append(ac.url_to_problem_name(url2))
         u.append(url2)
-    else:
-        q.append(ac.url_to_problem_name("https://atcoder.jp/contests/abc002/tasks/abc002_1")+" (サンプル問題2)")
-        u.append("https://atcoder.jp/contests/abc002/tasks/abc002_1")
     url3=st.text_input("問題ページのURLを入力してください(3問目)")
     if url3!="":
         q.append(ac.url_to_problem_name(url3))
         u.append(url3)
-    else:
-        q.append(ac.url_to_problem_name("https://atcoder.jp/contests/abc003/tasks/abc003_1")+" (サンプル問題3)")
-        u.append("https://atcoder.jp/contests/abc003/tasks/abc003_1")
     url4=st.text_input("問題ページのURLを入力してください(4問目)")
     if url4!="":
         q.append(ac.url_to_problem_name(url4))
         u.append(url4)
-    else:
-        q.append(ac.url_to_problem_name("https://atcoder.jp/contests/abc004/tasks/abc004_1")+" (サンプル問題4)")
-        u.append("https://atcoder.jp/contests/abc004/tasks/abc004_1")
     url5=st.text_input("問題ページのURLを入力してください(5問目)")
     if url5!="":
         q.append(ac.url_to_problem_name(url5))
         u.append(url5)
-    else:
-        q.append(ac.url_to_problem_name("https://atcoder.jp/contests/abc005/tasks/abc005_1")+" (サンプル問題5)")
-        u.append("https://atcoder.jp/contests/abc005/tasks/abc005_1")
+    url6=st.text_input("問題ページのURLを入力してください(6問目)")
+    if url6!="":
+        q.append(ac.url_to_problem_name(url6))
+        u.append(url6)
+
+def input_students():
+    global s
     student1=st.text_input("受講者のAtCoder IDを入力してください(1人目)")
     if student1!="":
         s.append(student1)
@@ -73,7 +67,7 @@ def addelement(df):
 
 def makegraph():
     name='問題名'
-    element=[q[0], q[1], q[2], q[3], q[4]]
+    element=q
     df=pd.DataFrame({
         name: element
     })
@@ -85,11 +79,31 @@ def makegraph():
 def printgraph(df):
     st.write(df)
 
-def main():
-    message()
+def manual():
     input_contents()
+    input_students()
     if st.button("進捗を確認する"):
         makegraph()
+
+def auto():
+    global q, s, u
+    course=st.radio("コースを選択してください", ("A-四則演算", "A-条件分岐","B-文字列", "B-forループ", "C-リスト", "S1-whileループ", "S1-多重ループ","S1-多次元リスト","S2-ソート関数","S2-再帰関数"))
+    if course=="A-四則演算":
+        for url in pb.A1_calc:
+            q.append(ac.url_to_problem_name(url))
+            u.append(url)
+    input_students()
+    if st.button("進捗を確認する"):
+        makegraph()
+
+def reset():
+    q.clear()
+    u.clear()
+    s.clear()
+
+def main():
+    message()
+    manual()
 
 if __name__=="__main__":
     main()
